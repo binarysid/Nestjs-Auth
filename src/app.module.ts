115ad956f412320156007865d65a4bf688a88d63
Linkon-Personal <siddique.linkon@gmail.com>
2025-01-10 00:17:17 +0600

    mongodb connection, schema

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [AuthModule, MongooseModule.forRoot('mongodb://localhost:27017/', 
+  { dbName: 'user'}
+  ), UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/user/user.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class User extends Document {
+    @Prop({
+        type: String,
+        isRequired: true,
+    })
+    name: string;
+    
+    @Prop({
+        type: String,
+        isRequired: true,
+    })
+    email: string;
+    
+    @Prop({
+        type: String,
+        isRequired: true,
+    })
+    password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
