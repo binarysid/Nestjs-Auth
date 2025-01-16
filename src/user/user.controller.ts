8b4416b7d856db50c5de8b727356725061e02d73
Linkon-Personal <siddique.linkon@gmail.com>
2025-01-17 01:00:50 +0600

    custom decorators [Making api access public]

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,12 +3,15 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { PatchUserDto } from './dtos/patch-user.dto';
 import { AccessTokenGuard } from 'src/auth/guards/access-token/access-token.guard';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { AuthType } from 'src/auth/enums/auth-type.enum';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('register')
+  @Auth(AuthType.None)
   public async create(@Body() dto: CreateUserDto) {
     console.log('type dto: ', typeof dto);
     console.log('instance dto: ', dto instanceof CreateUserDto);
